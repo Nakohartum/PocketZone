9ac58f328b4ba47bbabb95642b99110915959cb8
Nakohartum <58501792+Nakohartum@users.noreply.github.com>
2024-04-02 22:44:58 +0500

    Added player character and tested some movement. Imported some assets

---
+++ Assets/_Root/Code/Input/ShootController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Root.Code.Input
+{
+    public class ShootController 
+    {
+        public void Shoot()
+        {
+            Debug.Log("Fire");
+        }
+    }
+}
---
+++ Assets/_Root/Code/MoveFeature/MoveBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _Root.Code.MoveFeature
+{
+    public class MoveBehaviour
+    {
+        private Transform _transform;
+
+        public MoveBehaviour(Transform transform)
+        {
+            _transform = transform;
+        }
+
+        public void Move(Vector2 direction, float speed, float deltaTime)
+        {
+            _transform.Translate(direction * deltaTime * speed);
+        }
+    }
+}
